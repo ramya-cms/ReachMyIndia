fefff3e3e358920cee86cc94c161ad508d4fbb42
Sakshi-CMS <sakshi_bhavsar@cms.co.in>
2023-06-06 11:16:22 +0530

    "Add Excel sheet"
    Merge branch 'master' of https://github.com/Sakshi-CMS/ReachMyIndia.git

---
+++ src/main/java/Generic/Excel.java
@@ -0,0 +1,36 @@
+package Generic;
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.Reporter;
+public class Excel extends BaseTest{
+	
+
+		public static String Testdata(String Path, String Sheet, int row, int cell )
+		{
+			String v="";
+			try{
+				
+				FileInputStream fis = new FileInputStream(Path);
+				Workbook wb = WorkbookFactory.create(fis);
+				Sheet sh = wb.getSheet(Sheet);
+				Cell c = sh.getRow(row).getCell(cell);
+				v = c.getStringCellValue();
+				Reporter.log("Path is valid", true);
+					
+		}
+				
+			catch(Exception e)
+			{
+				Reporter.log("Invalid path", true);
+			}
+			return v;				
+		}
+
+	}
+
+
+
