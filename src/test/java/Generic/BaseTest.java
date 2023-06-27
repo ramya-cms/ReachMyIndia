1a3872adb6441a53d18e1d5ac9ef0d0fda805007
Sakshi-CMS <sakshi_bhavsar@cms.co.in>
2023-06-27 14:56:43 +0530

    "Failed ScreenShot and Extent Report is added"

--- src/test/java/Generic/BaseTest.java
+++ src/test/java/Generic/BaseTest.java
@@ -1,76 +1,79 @@
 package Generic;
 
-
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
 import java.io.FileInputStream;
 
 import java.io.IOException;
 import java.time.Duration;
-
+import java.time.LocalDateTime;
 import java.util.Properties;
 
+public class BaseTest implements Auto_Constant {
 
-public class BaseTest implements Auto_Constant
-{
+	public static WebDriver driver;
+	public Properties prop;
 
-	  protected static WebDriver driver;
-	  protected Properties prop;
-		
-		public BaseTest() {
-			//this.driver=driver;
-			PageFactory.initElements(driver, this);
-		}
-	
-		@BeforeSuite
-		public void initialize() throws IOException  {
-			
-			String path=System.getProperty("user.dir")+"//src//test//resources//ConfigFile//Config.properties";
-			
-			FileInputStream fin = new FileInputStream(path);
-			prop = new Properties();
-			prop.load(fin);
-			
+	public BaseTest() {
+		// this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@BeforeSuite
+	public void initialize() throws IOException {
+
+		String path = System.getProperty("user.dir") + "//src//test//resources//ConfigFile//Config.properties";
+
+		FileInputStream fin = new FileInputStream(path);
+		prop = new Properties();
+		prop.load(fin);
+
 		String strBrowser = prop.getProperty("browser");
-			
-		
-		if(strBrowser.equalsIgnoreCase("chrome")) {
+
+		if (strBrowser.equalsIgnoreCase("chrome")) {
 			driver = new ChromeDriver();
-		}
-		else if (strBrowser.equalsIgnoreCase("edge")) {
+		} else if (strBrowser.equalsIgnoreCase("edge")) {
 			driver = new EdgeDriver();
 		}
-		
-		
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(25));
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
-	
+
 		driver.get(prop.getProperty("url"));
 	}
+
 	
-	/*
-	 * @AfterMethod() public void tearDownMethod(ITestResult result) throws
-	 * IOException, InterruptedException {
-	 * 
-	 * if(result.getStatus()==ITestResult.FAILURE) { ScreenShot.screenshotFailure();
-	 * }
-	 * 
-	 * }
-	 */
-		   
-//	@AfterClass
-//	public void closeBrowser() {
-//		 driver.close();
-//	}
-    
-}
+    @AfterMethod(alwaysRun = true)
+   	public String getScreenshot(WebDriver driver, String testCaseName) throws IOException 
+    {
+       TakesScreenshot ts = (TakesScreenshot)driver;
+       File source = ts.getScreenshotAs(OutputType.FILE);
+       File file = new File(SFolderpath + String.join("_", LocalDateTime.now().toString().split("[^A-Za-z0-9]")) + ".png");
+      // File file=new File(SFolderpath+testCaseName+".png");
+       FileUtils.copyFile(source, file);
+       System.out.println("Screenshot Taken at "+System.currentTimeMillis());
+       return SFolderpath + ".png";
+     }
+
+
+// @AfterClass
+//     public void closeBrowser() {
+//	 driver.close();
+ }
+
+
+
---
+++ src/test/java/POM_Classes/TestNGListeners.java
@@ -0,0 +1,81 @@
+package POM_Classes;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import Generic.BaseTest;
+import Generic.ExtentReporterNG;
+
+public class TestNGListeners extends BaseTest implements ITestListener
+{	WebDriver driver;
+	ExtentTest test;
+	ExtentReports extent = ExtentReporterNG.getReportObject();
+
+	 @Override
+	    public void onTestStart(ITestResult result) {
+	       test= extent.createTest(result.getMethod().getMethodName());
+	    }
+
+	    @Override
+	    public void onTestSuccess(ITestResult result) {
+	        test.log(Status.PASS, "Test Passed");
+	    }
+
+	    @Override
+	    public void onTestFailure(ITestResult result) {
+	    	
+	    	test.fail(result.getThrowable());
+	    	try {
+				driver = (WebDriver) result.getTestClass().getRealClass().getField("driver")
+						.get(result.getInstance());
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			
+			}
+	    	
+	    	String filePath=null;
+			try {
+				filePath = getScreenshot(driver,result.getMethod().getMethodName());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	test.addScreenCaptureFromPath(filePath, result.getMethod().getMethodName());
+	       
+	    }
+
+	    @Override
+			public void onTestSkipped(ITestResult result) {
+			// TODO Auto-generated method stub
+			
+		}
+	    
+	    @Override
+		public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+			// TODO Auto-generated method stub
+			
+		}
+	    @Override
+		public void onStart(ITestContext context) {
+			// TODO Auto-generated method stub
+			
+		}
+	    
+		@Override
+		public void onFinish(ITestContext context) {
+			// TODO Auto-generated method stub
+			extent.flush();
+			
+		}
+		
+		
+}
